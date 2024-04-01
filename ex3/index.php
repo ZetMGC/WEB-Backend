9da59462cf6d4ad0dde8ba920d2c121a30bfcad6
ZetMGC <leontevdan798@gmail.com>
2024-04-01 15:08:41 +0300

    ex 3 fixed i hope x5

--- ex3/index.php
+++ ex3/index.php
@@ -94,7 +94,7 @@ foreach ($_POST['languages'] as $language) {
     }
 }
 try {
-    $stmt = $db->prepare("INSERT INTO info (names,tel,email,date,gender,bio)" . "VALUES (:fio,:tel,:email,:date,:gender,:bio)");
+    $stmt = $db->prepare("INSERT INTO info (fio,tel,email,date,gender,bio)" . "VALUES (:fio,:tel,:email,:date,:gender,:bio)");
     $stmt->execute(array('fio' => $fio, 'tel' => $tel, 'email' => $email, 'date' => $date, 'gender' => $gen, 'bio' => $bio));
     $info_id = $db->lastInsertId();
 
