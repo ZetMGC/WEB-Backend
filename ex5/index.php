2eaee97bc1919f7f62f9792d135ae33cd54ee906
ZetMGC <leontevdan798@gmail.com>
2024-05-18 14:38:33 +0300

    5 ex

--- ex5/index.php
+++ ex5/index.php
@@ -189,7 +189,7 @@ else {
     setcookie('date_value', $_POST['date'], time() + 30 * 24 * 60 * 60);
     include 'config.php';
 
-    $db = new PDO('mysql:host=127.0.0.1;dbname=u67314', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $db = new PDO('mysql:host=127.0.0.1;dbname=u67446', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if (empty($_POST['languages'])) {
