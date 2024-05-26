463c27c0c3171b63cf301d6f8696e0ac54f2aff2
ZetMGC <leontevdan798@gmail.com>
2024-05-27 00:57:59 +0300

    6 ex

--- ex6/admin.php
+++ ex6/admin.php
@@ -1,11 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
 <head>
+    <meta charset="UTF-8">
     <title>Задание 6</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
-
 <body>
 <?php
 include ('func.php');
-
 
 if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
     header('HTTP/1.1 401 Unauthorized');
@@ -22,8 +26,8 @@ $stmt = $db->prepare("SELECT * FROM admin WHERE admin_login = ?");
 $stmt->execute([$username]);
 $admin = $stmt->fetch();
 
-
-if (!$admin || !password_verify($password, $admin['admin_pass'])) {
+// Проверьте метод хеширования
+if (!$admin || !hash_equals($admin['admin_pass'], md5($password))) {
     header('HTTP/1.1 401 Unauthorized');
     header('WWW-Authenticate: Basic realm="My site"');
     print('<h1>401 Требуется авторизация</h1>');
@@ -31,20 +35,10 @@ if (!$admin || !password_verify($password, $admin['admin_pass'])) {
 }
 ?>
 
-
-<html lang="ru">
-
-<head>
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <title>Задание 6</title>
-</head>
-
-<body>
-<div class="form" style="width: 70%";>
+<div class="form" style="width: 70%;">
     <h2>Данные о пользователях</h2>
-    <table border="1">
+    <table class="table table-bordered">
+        <thead>
         <tr>
             <th>ID</th>
             <th>ФИО</th>
@@ -57,14 +51,14 @@ if (!$admin || !password_verify($password, $admin['admin_pass'])) {
             <th>Изменить</th>
             <th>Удалить</th>
         </tr>
+        </thead>
+        <tbody>
         <?php
-
         $sql = "SELECT i.id, i.fio, i.tel, i.email, i.date, i.gender, i.bio, GROUP_CONCAT(l.programming_language) AS programming_languages
-                FROM info i
-                LEFT JOIN info_language il ON i.id = il.info_id
-                LEFT JOIN programming_languages l ON il.language_id = l.id GROUP BY i.id";
+                    FROM info i
+                    LEFT JOIN info_language il ON i.id = il.info_id
+                    LEFT JOIN programming_languages l ON il.language_id = l.id GROUP BY i.id";
         $stmt = $db->query($sql);
-
 
         if ($stmt->rowCount() > 0) {
             while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -82,10 +76,13 @@ if (!$admin || !password_verify($password, $admin['admin_pass'])) {
                 echo "</tr>";
             }
         } else {
-            echo "<tr><td colspan='8'>No data available</td></tr>";
-        }      ?>
+            echo "<tr><td colspan='10'>Нет данных</td></tr>";
+        }
+        ?>
+        </tbody>
+    </table>
 
-    </table>   <?php
+    <?php
     $sql = "SELECT l.programming_language AS language, COUNT(il.language_id) AS count_users
             FROM programming_languages l
             LEFT JOIN info_language il ON l.id = il.language_id
@@ -94,17 +91,16 @@ if (!$admin || !password_verify($password, $admin['admin_pass'])) {
     $stmt = $db->prepare($sql);
     $stmt->execute();
 
-
     echo "<h2>Статистика :</h2>";
-    echo "<table border='1'>";
-    echo "<tr><th>Язык программирования</th><th>Количество пользователей</th></tr>";
+    echo "<table class='table table-bordered'>";
+    echo "<thead><tr><th>Язык программирования</th><th>Количество пользователей</th></tr></thead>";
+    echo "<tbody>";
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr><td>{$row['language']}</td><td>{$row['count_users']}</td></tr>";
     }
+    echo "</tbody>";
     echo "</table>";
-
     ?>
-
 </div>
 </body>
 </html>
